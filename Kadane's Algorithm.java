80601b23e375a038cdad63bcb52fde1df47d46fd
Ashu Sharma <81765508+ashusharma890@users.noreply.github.com>
2022-06-07 15:02:58 +0530

    Create Kadane's Algorithm.java

---
+++ Kadane's Algorithm.java
@@ -0,0 +1,19 @@
+// code start
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max_so_far=Integer.MIN_VALUE;
+        int max_ending=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            max_ending=max_ending+nums[i];
+            if(max_so_far < max_ending)
+                max_so_far=max_ending;
+            if(max_ending<0)
+                max_ending=0;
+        }
+        return max_so_far;
+    }
+}
+
+// code end
